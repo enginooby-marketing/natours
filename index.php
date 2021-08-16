9304bd06d983db58611d7cd2a300fb93ebb1c1b7
unknown <enginoobz@gmail.com>
2021-08-16 17:33:34 +0200

    [Hero] Animated button w/ hover effect (material)

--- index.php
+++ index.php
@@ -4,12 +4,17 @@
 <?php
 include_once __DIR__ . "/../../dui/_index.php";
 
-Head('Natours')->css('css/style.css')->show();
+Head('Natours')
+        ->css('css/style.css')
+        // FIX: decrete movement on page load if using animated class but load its css async
+        // ADHOC: load sync, or extract the used class from Animate.css
+        ->css('https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css')
+        ->show();
 // <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
 // <link rel="shortcut icon" type="image/png" href="img/favicon.png">
 ?>
 
-<body class="flat-style">
+<body class=" flat-style">
         <div class="display-table">
                 <div class="display-content p-0 ">
                         <div class="container m-0 hero-bg" id="hero">
@@ -18,11 +23,12 @@ Head('Natours')->css('css/style.css')->show();
                                         <img src="img/logo-white.png" alt="Logo" class="logo">
                                 </div>
 
-                                <div class="wrapper" id="hero-content">
+                                <div class="wrapper text-center" id="hero-content">
                                         <h1 class="heading-primary">
-                                                <span class="main"><span class="highlight-color ">Out</span>doors</span>
-                                                <span class="sub">is where <span class="highlight-color ">life</span> happens</span>
+                                                <span class="main animate__animated animate__fadeInLeft"><span class="highlight-color ">Out</span>doors</span>
+                                                <span class="sub animate__animated animate__fadeInRight">is where <span class="highlight-color ">life</span> happens</span>
                                         </h1>
+                                        <?php Button(label: "Discover our tours", wrapperClass: "mt-5 animate__animated animate__fadeInUp", class: "uppercase")->show() ?>
                                 </div>
 
                         </div>
