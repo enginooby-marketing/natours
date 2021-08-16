164c95332effb0e7c14b2b9ecc137edfa0b36fb1
unknown <enginoobz@gmail.com>
2021-08-16 10:50:46 +0200

    Starter resources

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+include_once __DIR__ . "/../../dui/_index.php";
+
+Head('Natours')->css('css/style.css')->show();
+// <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+// <link rel="shortcut icon" type="image/png" href="img/favicon.png">
+?>
+
+<body>
+        <!-- <script async src="../../dui/js/main.js" type="module"></script> -->
+</body>
+
+</html>
