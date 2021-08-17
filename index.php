af5c5364a0c6448e470db5c4ddf4556e4dc2780a
unknown <enginoobz@gmail.com>
2021-08-17 12:12:44 +0200

    [Refactor] Apply BEM & nest CSS selectors w/ SASS

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 include_once __DIR__ . "/../../dui/_index.php";
 
 Head('Natours')
+        ->meta(author: "enginoobz")
         ->css('css/style.css')
         // FIX: decrete movement on page load if using animated class but load its css async
         // ADHOC: load sync, or extract the used class from Animate.css
@@ -14,24 +15,26 @@ Head('Natours')
 // <link rel="shortcut icon" type="image/png" href="img/favicon.png">
 ?>
 
-<body class=" flat-style">
+<body class="flat-style ">
         <div class="display-table">
                 <div class="display-content p-0 ">
-                        <div class="container m-0 hero-bg" id="hero">
-                                <!-- REFACTOR: Image component -->
+                        <!-- TODO-DUI: make section component and customize individually in setting (add UI style in <section>, not <body>) -->
+                        <section class="container m-0 hero-bg" id="hero">
+                                <!-- REFACTOR-DUI: Image component -->
                                 <div class="image-wrapper" id="logo-wrapper">
                                         <img src="img/logo-white.png" alt="Logo" class="logo">
                                 </div>
 
-                                <div class="wrapper text-center" id="hero-content">
-                                        <h1 class="heading-primary">
-                                                <span class="main animate__animated animate__fadeInLeft"><span class="highlight-color ">Out</span>doors</span>
-                                                <span class="sub animate__animated animate__fadeInRight">is where <span class="highlight-color ">life</span> happens</span>
+                                <!-- REFACTOR - DUI: Headline component -->
+                                <div class="wrapper text-center headline">
+                                        <h1 class="headline__text">
+                                                <span class="headline__text--main animate__animated animate__fadeInLeft"><span class="highlight-color ">Out</span>doors</span>
+                                                <span class="headline__text--sub animate__animated animate__fadeInRight">is where <span class="highlight-color ">life</span> happens</span>
                                         </h1>
-                                        <?php Button(label: "Discover our tours", wrapperClass: "mt-5 animate__animated animate__fadeInUp", class: "uppercase")->show() ?>
+                                        <?php Button(label: "Discover our tours", wrapperClass: "mt-5 animate__animated animate__fadeInUp", class: "uppercase headline__btn")->show() ?>
                                 </div>
 
-                        </div>
+                        </section>
                 </div>
         </div>
         <script async src="../../dui/js/main.js" type="module"></script>
